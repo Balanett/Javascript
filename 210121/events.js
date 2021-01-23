7678e0c66be1224b1e6d0190cce1cabaa55a7960
Balázs Anett <71498781+Balanett@users.noreply.github.com>
2021-01-23 18:36:36 +0100

    take to js file

--- 210121/events.js
+++ 210121/events.js
@@ -6,12 +6,33 @@ function mouseOver() {
 function mouseOut() {
     document.getElementById("text").innerHTML = "This is a normal span";}
 
+document.getElementById("text").onmouseover = function() {mouseOver()};
+document.getElementById("text").onmouseout = function() {mouseOut()};
 
 
 // 3. feladat:
 
-let clicks = 0
+let clicks = -1
 function counter() {
     clicks += 1
     document.getElementById("counter").innerHTML = "Clicks counter: "+clicks;}
 
+counter()
+
+
+// 4. feladat:
+
+let button = document.getElementById('show-hide');
+let image = document.getElementById('pic');
+let imageDisplayed = false;
+
+button.addEventListener("dblclick", function () {
+
+    if (!imageDisplayed) {
+        image.style.display = "block";
+        imageDisplayed = true
+    } else {
+        image.style.display = "none";
+        imageDisplayed = false;
+    }
+})
